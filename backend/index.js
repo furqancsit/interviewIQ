b4fae7b7092f7e1cb2fdc852df840104b46cbe2c
furqan <furqancsit01@gmail.com>
2026-02-26 12:42:57 +0530

    initialize Express server and configure MongoDB connection

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import { app } from "./app.js";
+import connectDB from "./config/db.js";
+
+const PORT = process.env.PORT || 8000
+
+connectDB()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`⚙️ Server is running at port : ${PORT}`);
+        })
+    })
+    .catch((err) => {
+        console.log("MONGO db connection failed !!! ", err);
+
+    })
